0aa015689b88bb53b34904595deee24fdf4b7af4
xushankun <lovexsk@qq.com>
2019-07-24 14:12:19 +0800

    注释

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import user from './modules/user'
-import createPersistedPlugin from './plugins/createPersistedPlugin.js'
+import createPersistedPlugin from './plugins/createPersistedPlugin.js'      // 持久化存储store到
 const persistedPlugin = createPersistedPlugin()
 Vue.use(Vuex);
 export default new Vuex.Store({
