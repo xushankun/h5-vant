d11e75371f027ba8387e6f821522e4d5418b4cbc
xushankun <lovexsk@qq.com>
2019-12-04 14:07:56 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -17,30 +17,30 @@ export default {
   name: 'app',
   created() {
     // bind event
-    this.$navigation.on('forward', (to, from) => {
-      console.log('forward to', to, 'from ', from)
-    })
-    this.$navigation.on('back', (to, from) => {
-      console.log('back to', to, 'from ', from)
-    })
-    this.$navigation.on('replace', (to, from) => {
-      console.log('replace to', to, 'from ', from)
-    })
-    this.$navigation.on('refresh', (to, from) => {
-      console.log('refresh to', to, 'from ', from)
-    })
-    this.$navigation.on('reset', () => {
-      console.log('reset')
-    })
-    // and use [once, off] methods
-    this.$navigation.once('forward', () => {
-      console.log('appear once')
-    })
-    const off = () => {
-      console.log('will not appear')
-    }
-    this.$navigation.on('forward', off)
-    this.$navigation.off('forward', off)
+    // this.$navigation.on('forward', (to, from) => {
+    //   console.log('forward to', to, 'from ', from)
+    // })
+    // this.$navigation.on('back', (to, from) => {
+    //   console.log('back to', to, 'from ', from)
+    // })
+    // this.$navigation.on('replace', (to, from) => {
+    //   console.log('replace to', to, 'from ', from)
+    // })
+    // this.$navigation.on('refresh', (to, from) => {
+    //   console.log('refresh to', to, 'from ', from)
+    // })
+    // this.$navigation.on('reset', () => {
+    //   console.log('reset')
+    // })
+    // // and use [once, off] methods
+    // this.$navigation.once('forward', () => {
+    //   console.log('appear once')
+    // })
+    // const off = () => {
+    //   console.log('will not appear')
+    // }
+    // this.$navigation.on('forward', off)
+    // this.$navigation.off('forward', off)
   }
 }
 </script>
--- src/lib/API.js
+++ src/lib/API.js
@@ -1,20 +1,25 @@
 /**
  * Created by shankun on 2019/5/29.
  */
-// import { request } from './config'
-import request  from './request'
+import { request } from './config'
+// import request  from './request'
 
 export default {
-    // 登录
-    login: data=> request({
-        url: '/accesstoken',
-        method: 'post',
-        data
-    }),
-    // 获取用户详情
-    getUserData: params=> request({
-        url: '/user/'+ params.userName,
-        method: 'get',
-        params
-    })
+    // login: obj => request('/accesstoken', "post", obj),
+    // // 获取用户详情
+    // getUserData: obj => request('/user/'+ obj.userName, "get", obj),
+
+
+    // // 登录
+    // login: data=> request({
+    //     url: '/accesstoken',
+    //     method: 'post',
+    //     data
+    // }),
+    // // 获取用户详情
+    // getUserData: params=> request({
+    //     url: '/user/'+ params.userName,
+    //     method: 'get',
+    //     params
+    // })
 }
