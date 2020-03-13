e608bfa00aa31f509cc0b6ec366d0d9972843a8f
xushankun <lovexsk@qq.com>
2020-03-13 14:15:40 +0800

    企业状态变更bug修改

--- src/components/camera/inputCamera.vue
+++ src/components/camera/inputCamera.vue
@@ -6,12 +6,38 @@
          至于input是否好看，自己百度看如何美化...
        -->
     <div class="input-camera">
-        <div class="home">
-            <img alt="Vue logo" src="@/assets/logo.png"/>
-            <input ref="photoref" type="file" accept="image/*" @change="Photograph()" capture="camera"/>
-            <p>{{ fileName }}</p>
+        <div class="btn-block">
+            <div class="btn-box">
+                <input ref="photoFront" type="file" accept="image/*" @change="Photograph('photoFront')" capture="user"/>
+                <van-button type="primary">前置【拍照】</van-button>
+            </div>
+            <div class="btn-box">
+                <input ref="photorefEnd" type="file" accept="image/*" @change="Photograph('photorefEnd')" capture="camera"/>
+                <van-button type="primary">后置【拍照】</van-button>
+            </div>
+        </div>
+        <div class="btn-block">
+            <div class="btn-box">
+                <input ref="photoFront1" type="file" accept="video/*" @change="Photograph('photoFront1')" capture="user"/>
+                <van-button type="primary">前置【摄像】</van-button>
+            </div>
+            <div class="btn-box">
+                <input ref="photorefEnd1" type="file" accept="video/*" @change="Photograph('photorefEnd1')" capture="camera"/>
+                <van-button type="primary">后置【摄像】</van-button>
+            </div>
+        </div>
+        <div class="btn-block">
+            <div class="btn-box">
+                <input ref="photoFront2" type="file" accept="image/*,video/*" @change="Photograph('photoFront2')" capture="user"/>
+                <van-button type="primary">前置【拍照/摄像】</van-button>
+            </div>
+            <div class="btn-box">
+                <input ref="photorefEnd2" type="file" accept="image/*,video/*" @change="Photograph('photorefEnd2')" capture="camera"/>
+                <van-button type="primary">后置【拍照/摄像】</van-button>
+            </div>
         </div>
         <div class="preview">
+            <p v-if="fileName">fileName:{{ fileName }}</p>
             <img :src="base64ImgData" alt=""/>
         </div>
     </div>
@@ -22,7 +48,7 @@
         name: "inputCamera",
         data(){
             return {
-                fileName: '点击Vue拍照',
+                fileName: '',
                 base64ImgData: null
             }
         },
@@ -30,11 +56,11 @@
             /**
              * 获取用户拍照的图片信息
              */
-            async Photograph () {
+            async Photograph (refName) {
                 // 获取用户拍照的图片名字，显示到页面上
-                this.fileName = this.$refs.photoref.files[0].name
+                this.fileName = this.$refs[refName].files[0].name
                 // 获取图片base64 代码，并存放到 base64ImgData 中
-                this.base64ImgData = await this.FileReader(this.$refs.photoref.files[0])
+                this.base64ImgData = await this.FileReader(this.$refs[refName].files[0])
             },
             /**
              * 返回用户拍照图片的base64
@@ -54,26 +80,41 @@
 
 <style lang="less" scoped>
     .input-camera {
-        .home {
-            width: 200px;
-            margin: 0 auto;
-            position: relative;
-            input[type="file"] {
-                position: absolute;
-                left: 0;
-                top: 0;
-                width: 200px;
-                height: 200px;
-                z-index: 9;
-                opacity: 0;
-            }
-            p {
-                font-size: 13px;
-                color: #8d8d8d;
+        padding: 0 15px;
+        box-sizing: border-box;
+        .btn-block {
+            display: flex;
+            align-items: center;
+            padding: 10px 0;
+            .btn-box {
+                position: relative;
+                margin-right: 20px;
+                input[type="file"] {
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    width: 100%;
+                    height: 100%;
+                    z-index: 9;
+                    opacity: 0;
+                }
+                p {
+                    font-size: 14px;
+                    color: #333;
+                    height: 44px;
+                    display: flex;
+                    align-items: center;
+                }
             }
         }
-        .preview img {
-            width: 100%;
+
+        .preview {
+            p {
+                margin-bottom:20px ;
+            }
+            img {
+                width: 100%;
+            }
         }
     }
 
